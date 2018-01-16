4106f6abe5319666cdd5c9047dec0b404fa09f0d
Inder Singh <inder.prakash@hotmail.com>
2018-01-16 23:40:21 +0530

    f

--- src/main/java/com/mongo/MyFilter.java
+++ src/main/java/com/mongo/MyFilter.java
@@ -1,7 +1,5 @@
 package com.mongo;
-
 import java.io.IOException;
-
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
 import javax.servlet.FilterConfig;
@@ -12,12 +10,10 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 public class MyFilter implements Filter {
-
 	public void destroy() {
 		// TODO Auto-generated method stub
 		
 	}
-
 	public void doFilter(ServletRequest arg0, ServletResponse arg1, FilterChain arg2)
 			throws IOException, ServletException {
 		System.out.println("FILTER LOAD");
@@ -40,8 +36,7 @@ public class MyFilter implements Filter {
 	}
 
 	public void init(FilterConfig arg0) throws ServletException {
-		// TODO Auto-generated method stub
-		
+		// TODO Auto-generated method stub	
 	}
 
 }
