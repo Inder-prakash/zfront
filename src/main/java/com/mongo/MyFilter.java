0d676642ad7a5d019771c1585b941a6fc5914265
Inder Singh <inder.prakash@hotmail.com>
2018-01-16 23:22:21 +0530

    f

---
+++ src/main/java/com/mongo/MyFilter.java
@@ -0,0 +1,47 @@
+package com.mongo;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class MyFilter implements Filter {
+
+	public void destroy() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void doFilter(ServletRequest arg0, ServletResponse arg1, FilterChain arg2)
+			throws IOException, ServletException {
+		System.out.println("FILTER LOAD");
+        HttpServletRequest request = (HttpServletRequest) arg0;
+        System.out.println("CORSFilter HTTP Request: " + request.getMethod());
+ 
+   
+        ((HttpServletResponse) arg1).addHeader("Access-Control-Allow-Origin", "*");
+        ((HttpServletResponse) arg1).addHeader("Access-Control-Allow-Methods","GET, OPTIONS, HEAD, PUT, POST");
+ 
+        HttpServletResponse resp = (HttpServletResponse) arg1;
+ 
+        if (request.getMethod().equals("OPTIONS")) {
+            resp.setStatus(HttpServletResponse.SC_ACCEPTED);
+            return;
+        }
+ 
+        // pass the request along the filter chain
+        arg2.doFilter(request, arg1);
+	}
+
+	public void init(FilterConfig arg0) throws ServletException {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
